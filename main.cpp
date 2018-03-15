f7f778553c016e613b0e534ad22c870fb7069cb5
BobbyRockers <rrockers@gmail.com>
2018-03-14 19:17:51 -0500

    Added example code

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+	int As = 0;
+	int Fs = 0;
+	int as = 0;
+	int bs = 0;
+	int total = 0;
+	
+	cout << "Total A: " << As << endl;
+	cout << "Total F: " << Fs << endl;
+	cout << "Total a: " << as << endl;
+	cout << "Total b: " << bs << endl;
+	cout << "Total All Characters: " << total << endl;
+}
